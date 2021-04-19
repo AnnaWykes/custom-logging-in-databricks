54970e04a872eee8c3b37b5723cff7c8fd2800e1
Anna Wykes <wykesanna32@googlemail.com>
2021-04-19 10:25:28 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="cldbx", 
+    version="0.0.1",
+    author="Anna Wykes",
+    author_email="anna@advancinganalytics.com",
+    description="Custom Logging in Databricks",
+    long_description="Custom Logging in Databricks",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
